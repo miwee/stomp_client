5d08af9840bd3d58acda2b02ec4891354e24e414
miwee <miwee@nodomain.com>
2016-10-09 00:15:55 +0530

    published to hex

--- mix.exs
+++ mix.exs
@@ -7,6 +7,8 @@ defmodule StompClient.Mixfile do
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     description: description(),
+     package: package(),
      deps: deps()]
   end
 
@@ -27,6 +29,19 @@ defmodule StompClient.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [{:ex_doc, "~> 0.12", only: [:dev, :docs]}]
   end
+  
+  defp description do
+    "STOMP client for Elixir with broker specific addons"
+  end
+
+  defp package do
+    [# These are the default files included in the package
+     name: :stomp_client,
+     files: ["lib", "mix.exs", "README*", "LICENSE*"],
+     maintainers: ["miwee"],
+     licenses: ["Apache 2.0"],
+     links: %{"GitHub" => "https://github.com/miwee/stomp_client"}]
+  end  
 end
